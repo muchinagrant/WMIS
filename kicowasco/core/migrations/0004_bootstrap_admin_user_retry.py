41c258fed27834792d36a185e5300157d3a14926
GRANT <mchnaustin@gmail.com>
2026-03-10 01:40:12 +0300

    Retry bootstrap admin creation in production

---
+++ kicowasco/core/migrations/0004_bootstrap_admin_user_retry.py
@@ -0,0 +1,39 @@
+from django.db import migrations
+
+
+def create_bootstrap_admin(apps, schema_editor):
+    import os
+    from django.contrib.auth import get_user_model
+
+    username = os.environ.get("BOOTSTRAP_ADMIN_USERNAME")
+    email = os.environ.get("BOOTSTRAP_ADMIN_EMAIL")
+    password = os.environ.get("BOOTSTRAP_ADMIN_PASSWORD")
+
+    if not (username and email and password):
+        return
+
+    User = get_user_model()
+
+    if User.objects.filter(username=username).exists():
+        return
+
+    User.objects.create_superuser(
+        username=username,
+        email=email,
+        password=password,
+    )
+
+
+def noop(apps, schema_editor):
+    pass
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("core", "0003_bootstrap_admin_user"),
+    ]
+
+    operations = [
+        migrations.RunPython(create_bootstrap_admin, noop),
+    ]
